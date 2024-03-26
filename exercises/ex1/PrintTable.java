41a289e7e2b3e1ce88c7ac6bf960f5a41a0c8f07
Listen-to-your-heart <163817883+Listen-to-your-heart@users.noreply.github.com>
2024-03-26 10:23:40 +0800

    Create PrintTable.java

---
+++ exercises/ex1/PrintTable.java
@@ -0,0 +1,17 @@
+package exercise1;
+
+class PrintTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i <= 9; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(j + "×" + i + "=" + i * j + " ");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+}
